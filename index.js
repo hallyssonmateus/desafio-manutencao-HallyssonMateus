6bf20ea2d00382afe34c5ba33f2d1a344cd5c903
hallyssonmateus <hallysson_gato@hotmail.com>
2024-08-25 05:38:15 -0300

    feat: Implementando um carrossel

--- index.js
+++ index.js
@@ -9,6 +9,24 @@ $(document).ready(function(){
     });
 });
 
+//Banner - Slider
+var radio = document.querySelector('.manual-btn')
+var cont = 1;
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 5000)
+
+function proximaImg(){
+    cont++
+    if (cont >3){
+        cont = 1
+    }
+
+    document.getElementById('radio'+cont).checked = true
+
+}
 //Função abrir modal
 const openModal = () => {
     modal.classList.remove("hide");
